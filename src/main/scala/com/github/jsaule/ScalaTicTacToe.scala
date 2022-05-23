02824d7cd8445c9095fb7a80da1faee89c4adc67
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-05-23 12:50:59 +0300

    added more comments

--- src/main/scala/com/github/jsaule/ScalaTicTacToe.scala
+++ src/main/scala/com/github/jsaule/ScalaTicTacToe.scala
@@ -4,7 +4,7 @@ import scala.io.StdIn.readLine
 import scala.util.Random
 
 class ScalaTicTacToe {
-  // intro + initial input from players
+  // intro + initial input from players (real player one vs. real player two OR real player one vs. computer)
   println("Welcome to the Tic Tac Toe Game!")
   val inputNameOne: String = readLine("What is your name, Player 1? ")
   val inputXO: String = readLine("Please choose X or O. ")
@@ -15,8 +15,10 @@ class ScalaTicTacToe {
   if (inputNameTwo == "") inputNameTwo = "Computer"
   println(s"Thank you, $inputNameTwo. Your symbol is '$playerTwo'.")
 
-  // starting board positions + sets necessary to win
+  // starting board all possible positions
   val emptyBoard: Array[Char] = Array('1', '2', '3', '4', '5', '6', '7', '8', '9')
+
+  // all winning board positions sets
   val winningSets: Set[Set[Int]] = Set(
     Set(0, 1, 2),
     Set(3, 4, 5),
@@ -32,7 +34,7 @@ class ScalaTicTacToe {
   printBoard(emptyBoard)
   nextTurn(emptyBoard)
 
-  // defining who's turn it is (human one, human two, computer) and requesting input
+  // defining who's turn it is (human one, human two, computer) and requesting move input
   def nextTurn(board: Array[Char]): Unit = {
     val remainingTurns = board.count(_.toString.matches("[1-9]"))
     val next = if(remainingTurns%2 == 0) playerTwo.toCharArray.head else playerOne.toCharArray.head
@@ -44,12 +46,13 @@ class ScalaTicTacToe {
     }
   }
 
-  // computer's input (if it's game human vs. computer)
+  // returns move with computer's input (if it's game human vs. computer)
   def computerInput(board: Array[Char]): Int = {
     val input = Random.nextInt(9).toString
     if (input.matches("[1-9]")) {
       val move = input.toInt -1
       if (board(move).isDigit) {
+        println("Computer made a move.")
         move
       } else {
         computerInput(board)
@@ -59,9 +62,9 @@ class ScalaTicTacToe {
     }
   }
 
-  // human player's input (human one or human two)
+  // returns move with human player's input (human one or human two)
   def moveInput(board: Array[Char]): Int = {
-    val input = readLine("Enter your board position number to move (between 1 and 9). ")
+    val input = readLine("Enter your board position number to move (between 1 and 9), player "+nextPlayer(board)+"! ")
     if (input.matches("[1-9]")) {
       val move = input.toInt -1
       if (board(move).isDigit) {
@@ -76,14 +79,14 @@ class ScalaTicTacToe {
     }
   }
 
-  // calling next player
+  // returns next player
   def nextPlayer(board: Array[Char]): Char = {
     val remainingTurns = board.count(_.toString.matches("[1-9]"))
     val next = if(remainingTurns%2 == 0) playerTwo.toCharArray.head else playerOne.toCharArray.head
     next
   }
 
-  // printing board
+  // prints board
   def printBoard(board: Array[Char]): Unit = {
     println(
       0 to 2 map { r =>
@@ -92,16 +95,17 @@ class ScalaTicTacToe {
         } mkString " | "
       } mkString ("\n", "\n—   —   —\n", "\n")
     )
-    println("\nNext Player is " + nextPlayer(board))
   }
 
-  // checking if it's win (saving results in data base (creating it and a table if they don't exist) in that case) or forcing game to continue
+  /* returns true or false if it's win or draw and
+  saves results (win or draw) in data base (creating it and tables
+  if they don't exist) if it's win or draw */
   def checkWin(board: Array[Char]): Boolean = {
     winningSets.foreach(pattern=>{
       if(pattern.forall(board(_) == board(pattern.head))) {
         val winnerName = if (board(pattern.head).toString == playerOne) s"$inputNameOne" else s"$inputNameTwo"
         val loserName = if (board(pattern.head).toString == playerOne) s"$inputNameTwo" else s"$inputNameOne"
-        println(s"\nWinner is ${board(pattern.head)}! Congratulations, $winnerName!")
+        println(s"Winner is ${board(pattern.head)}! Congratulations, $winnerName!")
         println(s"Better luck next time, $loserName!")
         println("\nGame result saved.")
         val db = new WinnersLosersDB("src/resources/db/winners_losers.db")
@@ -110,14 +114,14 @@ class ScalaTicTacToe {
         db.connect.close()
         return true
       }
-        else if(board.count(_.toString.matches("[1-9]")) == 0) {
+        else if(board.count(_.toString.matches("[1-9]")) <= 0) {
         val playerOneName = inputNameOne
         val playerTwoName = inputNameTwo
         val db = new WinnersLosersDB("src/resources/db/winners_losers.db")
         db.migrateDraw()
         db.insertDrawResults(playerOneName, playerTwoName)
         db.connect.close()
-        println(s"It's a draw! Congratulations both, $playerOneName and $playerTwoName!")
+        println(s"It's a draw! Well played both, $playerOneName and $playerTwoName!")
         println("\nGame result saved.")
         return true
       }
@@ -128,7 +132,8 @@ class ScalaTicTacToe {
 }
 
 object ScalaTicTacToe extends App {
-  //starting new game
+
+  //creates new game (uses ScalaTicTacToe class)
   val game = new ScalaTicTacToe
 
 }
